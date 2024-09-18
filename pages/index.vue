588a8a8319d3c2cf98f1c08699e5bbac3e095332
Aleksandr Mykulych <oleksandr.mykulych@tentens.tech>
2024-09-18 20:11:16 +0300

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="bg-red-200 h-32">
+    <div class="flex flex-justify-center flex-items-center h-full">
+      Index Page
+    </div>
+  </main>
+</template>
---
+++ uno.config.ts
@@ -0,0 +1,35 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    colors: {
+      // ...
+    }
+  },
+  presets: [
+    presetUno(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        // ...
+      },
+    }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
+})
